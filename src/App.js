f8fae92d9fb621ca92e3b58f5942bc164879cb02
Ronald3217 <megasnake305@gmail.com>
2021-11-11 19:14:07 -0600

    update package with customTheme

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const Button = styled.input`
   background-color: #fff;
   font-weight: bold;
   margin: 20px;
-  
+
   &:hover {
     cursor: pointer;
   }
@@ -37,71 +37,160 @@ const ButtonPurple = styled(Button)`
   background-color: rgb(150, 134, 255);
   color: #fff;
 `;
+const ButtonCustom = styled(Button)`
+  background-color: #fff;
+  color: #000;
+`;
+const InputContainer = styled.div`
+  margin: 5px;
+`;
+const TextInput = styled.input`
+  max-width: 400px;
+  width: 100%;
+  height: 56px;
+  border-radius: 4px;
+  position: relative;
+  background-color: #fff;
+  transition: 0.3s all;
+`;
+const Link = styled.a`
+  text-decoration: none;
+  color: #000;
+  font-weight: bold;
+  text-transform: capitalize;
+
+  & span {
+    color: #fff;
+    text-transform: none;
+  }
+`;
 
 function App() {
-   const initialData = [
+  const initialData = [
     {
-      title: "Heading Number 1?",
-      paragraph: "Some Answer Here"
+      title: 'Heading Number 1?',
+      paragraph: 'Some Answer Here',
     },
     {
-      title: "Heading Number 2",
-      paragraph:
-        "lorem ipsum dolor sit amet lorem ipsum lorem ips"
+      title: 'Heading Number 2',
+      paragraph: 'lorem ipsum dolor sit amet lorem ipsum lorem ips',
     },
     {
-      title: "Heading Number 3",
-      paragraph:
-        "lorem ipsum dolor sit amet lorem ipsum lorem ips"
+      title: 'Heading Number 3',
+      paragraph: 'lorem ipsum dolor sit amet lorem ipsum lorem ips',
     },
     {
-      title: "Heading Number 4",
-      paragraph:
-        "Some Paragraph Here"
-    }
+      title: 'Heading Number 4',
+      paragraph: 'Some Paragraph Here',
+    },
   ];
   const [theme, setTheme] = useState('green');
+  const [customTheme, setCustomTheme] = useState({});
   const [data, setData] = useState(initialData);
 
-  const handleThemeChange = (e)=>{
+  const handleThemeChange = (e) => {
     console.log(e.target.name);
-    setTheme(e.target.name)
-  }
+    setTheme(e.target.name);
+  };
+  const handleCustomThemeActivate = (e) => {
+    setTheme(e.target.name);
+  };
+  const handleCustomThemeChange = (e) => {
+    setCustomTheme({
+      ...customTheme,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div className="App">
       <ButtonsContainer>
-        <Button 
-        type="button" value="Default"
-        name=""
-        onClick={handleThemeChange}
-      />
-      <ButtonDark 
-        type="button" value="Dark"
-        name="dark"
-        onClick={handleThemeChange}
-      />
-      <ButtonNetflix
-        type="button" value="Netflix"
-        name="netflix"
-        onClick={handleThemeChange}
-      />
-      <ButtonGreen 
-        type="button" value="Green"
-        name="green"
-        onClick={handleThemeChange}
-      />
-      <ButtonBlue
-        type="button" value="Blue"
-        name="blue"
-        onClick={handleThemeChange}
-      />
-      <ButtonPurple
-        type="button" value="Purple"
-        name="purple"
-        onClick={handleThemeChange}
-      />
+        <Button
+          type="button"
+          value="Default"
+          name=""
+          onClick={handleThemeChange}
+        />
+        <ButtonDark
+          type="button"
+          value="Dark"
+          name="dark"
+          onClick={handleThemeChange}
+        />
+        <ButtonNetflix
+          type="button"
+          value="Netflix"
+          name="netflix"
+          onClick={handleThemeChange}
+        />
+        <ButtonGreen
+          type="button"
+          value="Green"
+          name="green"
+          onClick={handleThemeChange}
+        />
+        <ButtonBlue
+          type="button"
+          value="Blue"
+          name="blue"
+          onClick={handleThemeChange}
+        />
+        <ButtonPurple
+          type="button"
+          value="Purple"
+          name="purple"
+          onClick={handleThemeChange}
+        />
       </ButtonsContainer>
-      <Accordion data={data} theme={theme} />
+      <Accordion data={data} theme={theme} customTheme={customTheme} />
+      <ButtonsContainer>
+        <ButtonCustom
+          type="button"
+          value="Custom"
+          name="custom"
+          onClick={handleCustomThemeActivate}
+        />
+        <InputContainer>
+          <Link
+            href="https://github.com/Ronald3217/react-acordion"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read How To Use, in <span>Github Readme.md</span>.
+          </Link>
+        </InputContainer>
+        <InputContainer>
+          <TextInput
+            type="text"
+            name="backgroundHeader"
+            placeholder="color hexadecimal, rgb o rgba ex: #ffaf00"
+            onChange={handleCustomThemeChange}
+          />
+        </InputContainer>
+        <InputContainer>
+          <TextInput
+            type="text"
+            name="background"
+            placeholder="color hexadecimal, rgb o rgba ex: #ffe4a9"
+            onChange={handleCustomThemeChange}
+          />
+        </InputContainer>
+        <InputContainer>
+          <TextInput
+            type="text"
+            name="color"
+            placeholder="color hexadecimal, rgb o rgba ex: #000"
+            onChange={handleCustomThemeChange}
+          />
+        </InputContainer>
+        <InputContainer>
+          <TextInput
+            type="text"
+            name="borderBottom"
+            placeholder=".5px solid #fff"
+            onChange={handleCustomThemeChange}
+          />
+        </InputContainer>
+      </ButtonsContainer>
     </div>
   );
 }
